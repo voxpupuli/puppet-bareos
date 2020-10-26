0eca74839b74da36d4a4f84f242311b6c9e9b314
Benedikt Braunger <b.braunger@syseleven.de>
2020-10-26 17:54:56 +0100

    added .with_all_deps

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,7 +7,7 @@ describe 'bareos' do
       end
 
       context 'with default values for all parameters' do
-        it { is_expected.to compile }
+        it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('bareos') }
       end
 
