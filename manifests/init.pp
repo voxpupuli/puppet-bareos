06e37b06d75264742ac476fc57f053de58b96253
Robert Wolf <robert.wolf@abacus.ch>
2021-03-31 14:37:43 +0200

    update to upstream version

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,13 +30,13 @@
 #   Name of the package which should be installed
 #
 class bareos (
-  $config_dir                         = $::bareos::params::config_dir,
-  $config_dir_webui                   = $::bareos::params::config_dir_webui,
-  $file_owner                         = $::bareos::params::file_owner,
-  $file_group                         = $::bareos::params::file_group,
-  $file_mode                          = $::bareos::params::file_mode,
-  $file_dir_mode                      = $::bareos::params::file_dir_mode,
-  $user_groups                        = $::bareos::params::user_groups,
+  $config_dir                         = $bareos::params::config_dir,
+  $config_dir_webui                   = $bareos::params::config_dir_webui,
+  $file_owner                         = $bareos::params::file_owner,
+  $file_group                         = $bareos::params::file_group,
+  $file_mode                          = $bareos::params::file_mode,
+  $file_dir_mode                      = $bareos::params::file_dir_mode,
+  $user_groups                        = $bareos::params::user_groups,
   String  $repo_release               = 'latest',
   Boolean $repo_subscription          = false,
   Optional[String[1]]  $repo_username = undef,
@@ -51,15 +51,15 @@ class bareos (
   Boolean $manage_user                = true,
   String  $package_name               = 'bareos-common',
 
-  $console_package_name  = $::bareos::params::console_package_name,
-  $monitor_package_name  = $::bareos::params::monitor_package_name,
-  $director_package_name = $::bareos::params::director_package_name,
-  $director_service_name = $::bareos::params::director_service_name,
-  $director_managed_dirs = $::bareos::params::director_managed_dirs,
-  $client_package_name   = $::bareos::params::client_package_name,
-  $client_service_name   = $::bareos::params::client_service_name,
-  $storage_package_name  = $::bareos::params::storage_package_name,
-  $storage_service_name  = $::bareos::params::storage_service_name,
+  $console_package_name  = $bareos::params::console_package_name,
+  $monitor_package_name  = $bareos::params::monitor_package_name,
+  $director_package_name = $bareos::params::director_package_name,
+  $director_service_name = $bareos::params::director_service_name,
+  $director_managed_dirs = $bareos::params::director_managed_dirs,
+  $client_package_name   = $bareos::params::client_package_name,
+  $client_service_name   = $bareos::params::client_service_name,
+  $storage_package_name  = $bareos::params::storage_package_name,
+  $storage_service_name  = $bareos::params::storage_service_name,
 ) inherits bareos::params {
   if $manage_repo {
     class { 'bareos::repository':
