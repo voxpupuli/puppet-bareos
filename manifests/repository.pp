f3698fa0e8c49208fe37eab0816979d32b51555e
Ward <ward@calrissia.com>
2022-11-01 12:56:28 +0100

    Include matejzero's fix for EL7

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -86,10 +86,17 @@ class bareos::repository (
         'Centos', 'Rocky', 'AlmaLinux': {
           if (
             (
+<<<<<<< HEAD
               versioncmp($release, '21') >= 0 and
               versioncmp($osmajrelease, '8')
             ) or ($release == 'current')
             ) {
+=======
+              (versioncmp($release, '21') >= 0) or
+              ($release == 'current')
+            ) and (versioncmp($osmajrelease, '8') >= 0)
+          ) {
+>>>>>>> 852a600 (Include matejzero's fix for EL7)
             $location = "${url}EL_${osmajrelease}"
           } else {
             $location = "${url}CentOS_${osmajrelease}"
