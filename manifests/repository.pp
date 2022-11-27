69f9b9b66f3e86f317bde75b356ff2822e9a4be2
Ward <ward.hus@calrissia.com>
2022-11-27 23:28:17 +0100

    Current should have the same repo as 21

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -86,9 +86,10 @@ class bareos::repository (
         'Centos', 'Rocky', 'AlmaLinux': {
           if (
             (
-              (versioncmp($release, '21') >= 0) and
-              (versioncmp($osmajrelease, '8') >= 0)
-            ) or ($release == 'current')
+              (versioncmp($release, '21') >= 0) or
+              ($release == 'current')
+            ) and
+            (versioncmp($osmajrelease, '8') >= 0)
           ) {
             $location = "${url}EL_${osmajrelease}"
           } else {
